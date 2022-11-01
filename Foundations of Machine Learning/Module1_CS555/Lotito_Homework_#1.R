f2e427db88e3e6ff9608fef05a650e6da023edb9
davelotito <DavidJLotito@gmail.com>
2022-11-01 11:53:17 -0400

    dataset loaded

--- Foundations of Machine Learning/Module1_CS555/Lotito_Homework_#1.R
+++ Foundations of Machine Learning/Module1_CS555/Lotito_Homework_#1.R
@@ -2,3 +2,8 @@
 # MET CS 555
 
 library(tidyverse)
+
+setwd('/Users/davelotito/Desktop/MET CS555 - Foundations of Machine Learning/Assignments/Assignment1')
+c.difficile <- read.csv("dataset_module1.csv", header = FALSE)
+c.difficile <- unlist(c.difficile, use.names = FALSE)
+
