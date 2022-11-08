8313221d83b35bd396d90c1b7b60c62adf1e0b29
davelotito <DavidJLotito@gmail.com>
2022-11-08 08:14:01 -0500

    Question #1

--- Foundations of Machine Learning/Module2_CS555/Lotito_Homework_#2.R
+++ Foundations of Machine Learning/Module2_CS555/Lotito_Homework_#2.R
@@ -1,3 +1,20 @@
 # (1) Save the data to a CSV file and read into R for analysis. 
 setwd('/Users/davelotito/Desktop/MET CS555 - Foundations of Machine Learning/Assignments/Assignment2')
-cal_info <- read.csv("Calorie_Info.csv", header = FALSE)
+cal_info <- read.csv("Calorie_Info.csv", header = T)
+
+# 1) Summarize the data by whether children participated in the meal preparation or not.  
+# Use an appropriately labelled table to show the results.  
+# Also include a graphical presentation that shows the distribution of calories for participants vs. non-participants.  
+# Describe the shape of each distribution and comment on the similarity (or lack thereof) 
+# between the distributions in each group.
+cal_info
+aggregate(cal_info, by=list(cal_info$Participants, cal_info$Non_Participants), FUN=summary)
+
+par(mfrow = c(1, 2))
+hist(cal_info$Participant, main = "Histogram of Participants", col = blues9)
+hist(cal_info$Non_Participants, main= "Histogram of Non-Participants", col = blues9)
+
+# The shape of the two distributions are not normal distributions, we can see
+# That these two distributions are no similar as the calories for non-participant spiked
+# between 300-500 calories indicating that non-participants are likely to have more calories
+# vs. the participants in the study.
