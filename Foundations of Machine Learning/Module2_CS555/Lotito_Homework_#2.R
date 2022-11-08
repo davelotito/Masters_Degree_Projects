5cab447b0d04be3075877991a4481d8394b929e3
davelotito <DavidJLotito@gmail.com>
2022-11-08 07:37:24 -0500

    Start of hw #2

---
+++ Foundations of Machine Learning/Module2_CS555/Lotito_Homework_#2.R
@@ -0,0 +1,3 @@
+# (1) Save the data to a CSV file and read into R for analysis. 
+setwd('/Users/davelotito/Desktop/MET CS555 - Foundations of Machine Learning/Assignments/Assignment2')
+cal_info <- read.csv("Calorie_Info.csv", header = FALSE)
