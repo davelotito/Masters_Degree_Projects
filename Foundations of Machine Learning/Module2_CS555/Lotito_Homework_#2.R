a6128e77a1a2b88fc140f7f766b2c316543d3a04
davelotito <DavidJLotito@gmail.com>
2022-11-10 10:18:32 -0500

    Updates for homework

--- Foundations of Machine Learning/Module2_CS555/Lotito_Homework_#2.R
+++ Foundations of Machine Learning/Module2_CS555/Lotito_Homework_#2.R
@@ -1,18 +1,26 @@
-# (1) Save the data to a CSV file and read into R for analysis. 
-setwd('/Users/davelotito/Desktop/MET CS555 - Foundations of Machine Learning/Assignments/Assignment2')
-cal_info <- read.csv("Calorie_Info.csv", header = T)
-
 # 1) Summarize the data by whether children participated in the meal preparation or not.  
 # Use an appropriately labelled table to show the results.  
 # Also include a graphical presentation that shows the distribution of calories for participants vs. non-participants.  
 # Describe the shape of each distribution and comment on the similarity (or lack thereof) 
 # between the distributions in each group.
-cal_info
-aggregate(cal_info, by=list(cal_info$Participants, cal_info$Non_Participants), FUN=summary)
 
-par(mfrow = c(1, 2))
-hist(cal_info$Participant, main = "Histogram of Participants", col = blues9)
-hist(cal_info$Non_Participants, main= "Histogram of Non-Participants", col = blues9)
+Participants <- c(230.16,210.99,288.73,590.28,582.59,635.21,249.86,441.66,572.43,357.78,
+                  396.79,298.38,282.99,368.51,388.59,256.32,
+                  408.8,424.94,477.96,428.74,432.52,428.27,
+                  596.79,456.30,446.38)
+
+Non_Participants <- c(614.61,503.46,425.22,688.77,184,299.73,350.65,394.94,261.55,295.28,
+                     139.69,462.78,179.59,301.75,436.58,371.39,
+                     469.02,378.09,287.31,448.55,332.64,403.98)
+
+
+summary(Participants)
+
+summary(Non_Participants)
+
+
+hist(Participants, main = "Histogram of Participants", col = blues9)
+hist(Non_Participants, main= "Histogram of Non-Participants", col = blues9)
 
 # The shape of the two distributions are not normal distributions, we can see
 # That these two distributions are not similar as the calories for non-participant spiked
@@ -21,4 +29,38 @@ hist(cal_info$Non_Participants, main= "Histogram of Non-Participants", col = blu
 # 200-300 calories and then again at 400-500 calories. From our initial summary we can see that the sum of 
 # total calories for participant vs. non-participant shows  us that Non_Participants consumed less overall calories
 # but we also had less observations with 3 missing.
+
+# (2) Does the mean calorie consumption for those who participated in the 
+# meal preparation differ from 425?  
+# Formally test at the alpha = 0.05 level using the 5 steps outlined in the module. 
+
+# Steps
+# 1) Set up the hypotheses and select the alpha level
+# 2) Select the appropriate test statistic
+# 3) State the decision rule
+# 4) Compute the test statistic and the associated p-value
+# 5) State your conclusion
+abs(qt(0.05,24))
+
+t.test(Participants,mu=425,alternative="two.sided")
+
+# (3) Calculate a 90% confidence interval for the mean calorie intake for participants in the meal 
+# preparation. Interpret the confidence interval. 
+abs(qt(0.05,45))
+
+t.test(Participants,conf.level=0.90)
+
+# (4) Formally test whether or not participants consumed more calories than non-participants at the 
+# alpha = 0.05 level using the 5 steps outlined in the module.
+t.test(Participants,Non_Participants)
+
+
+
+
+
+
+
+
+
+
 
