5bae3afc0dde96aaefa3b25c0dbcfdb802f5f9d3
davelotito <DavidJLotito@gmail.com>
2022-11-19 20:27:10 -0500

    EDA

--- Foundations of Machine Learning/Term_Project/Term_Project_CS555.R
+++ Foundations of Machine Learning/Term_Project/Term_Project_CS555.R
@@ -1,3 +1,15 @@
 # David Lotito
 # CS555 - Term Project Fall 2022
 
+setwd('/Users/davelotito/Code/Masters_Degree_Projects/Foundations of Machine Learning/Term_Project')
+
+heart_data <- read.csv('heart.csv', header = T)
+
+View(heart_data)
+
+attach(heart_data)
+
+hist(Age)
+
+
+
