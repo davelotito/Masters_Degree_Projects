2a258993d7fa6b44af286b20ecc1c53672ca26df
davelotito <DavidJLotito@gmail.com>
2022-11-22 07:12:40 -0500

    Data import

--- Foundations of Machine Learning/Module4_CS555/Lotito_Homework_#4.R
+++ Foundations of Machine Learning/Module4_CS555/Lotito_Homework_#4.R
@@ -1,2 +1,6 @@
 # David Lotito
 # MET CS 555
+
+# Save the data to a CSV file and read into R for analysis. 
+setwd('/Users/davelotito/Code/Masters_Degree_Projects/Foundations of Machine Learning/Module4_CS555/')
+data <- read.csv("Assignment4_Data.csv", header = T)
