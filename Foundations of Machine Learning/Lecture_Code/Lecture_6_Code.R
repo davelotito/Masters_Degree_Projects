cd147f2d28398f94f10e2fd33fc6218eaaadd0fa
davelotito <DavidJLotito@gmail.com>
2022-12-04 09:09:44 -0500

    Last module, start of quiz prep

---
+++ Foundations of Machine Learning/Lecture_Code/Lecture_6_Code.R
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################
+### QUIZ #6 ######
+##################
+
+# Question #2
+
+#A study of stroke patients who survived 6 months after the stroke found that 6/45 men and 
+# 22/63 women lived in an institution (e.g., nursing home or assisted living facility). 
+# What is the odds ratio of living in an institution between men and women 
+# (using men as the reference group)?
+
+
+women <- 22/63
+men <- 6/45
+
+(women/(1-women)) / (men/(1-men))
+
+
+
+
+
+
+
+
+
+
+
