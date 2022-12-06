24244de6c9fef084376378b696b5c34133ba0d78
davelotito <DavidJLotito@gmail.com>
2022-12-06 07:57:45 -0500

    Quiz prep wrap up

--- Foundations of Machine Learning/Lecture_Code/Lecture_6_Code.R
+++ Foundations of Machine Learning/Lecture_Code/Lecture_6_Code.R
@@ -93,3 +93,86 @@ p2 <- (6/45)
 (((22/63)-(6/45)) + 1.96 * sqrt(((p1*(1-p1))/63) + ((p2*(1-p2))/45)))*100
 
 
+# Question #7
+
+# We have 30-day follow-up data on 350 stroke patients and want to investigate whether the risk 
+# of recurrent stroke and/or death depends on the type of stroke (cerebral embolism or not). 
+# The results of the simple logistic regression of the dummy variable for cerebral embolism 
+# (1 = yes, 0 = no) are shown below. Use the output to calculate the odds ratio for 
+# recurrent stroke and/or death for those who had a cerebral embolism versus those who did not?
+
+exp(1.87)
+
+# Question #8
+
+# We have 30-day follow-up data on 350 stroke patients and want to investigate whether 
+# the risk of recurrent stroke and/or death depends on the type of stroke (cerebral embolism or not) 
+# and age. The results of the multiple logistic regression of the dummy variable for cerebral embolism 
+# (1 = yes, 0 = no) and age are shown below. 
+# What is the risk of recurrent stroke/death for a patient without a cerebral 
+# embolism who is 60 years of age?
+
+exp(-15.32+0.18*60)
+
+# Question #9
+
+# We have 30-day follow-up data on 350 stroke patients and want to investigate 
+# whether the risk of recurrent stroke and/or death depends on the type of stroke 
+# (cerebral embolism or not) and age. 
+# The results of the multiple logistic regression of the dummy variable 
+# for cerebral embolism (1 = yes, 0 = no) and age are shown below. Calculate the
+#  odds ratio comparing the odds of recurrent stroke/death for a patient who is 65 versus 64, 
+# controlling for cerebral embolism.
+
+exp(0.18)*(65-64)
+
+# Question #10
+
+# We have 30-day follow-up data on 350 stroke patients and want to investigate whether the 
+# risk of recurrent stroke and/or death depends on the type of stroke (cerebral embolism or not) 
+# and age. The results of the multiple logistic regression of the dummy variable for 
+# cerebral embolism (1 = yes, 0 = no) and age are shown below. 
+# Calculate the odds ratio comparing the odds of recurrent stroke/death for 
+# a patient who is 65 versus 55, controlling for cerebral embolism.
+
+b1 <- 0.18*(65-55)
+
+exp(b1)
+
+# Question #11
+
+# We have 30-day follow-up data on 350 stroke patients and want to investigate whether the risk 
+# of recurrent stroke and/or death depends on the type of stroke (cerebral embolism or not) and age. 
+# The results of the multiple logistic regression of the dummy variable for cerebral embolism 
+# (1 = yes, 0 = no) and age are shown below. Calculate the 95% confidence interval for the odds ratio 
+# comparing the odds of recurrent stroke/death for patients with a cerebral 
+# embolism versus those without, controlling for age.
+
+exp(2.07-1.960*0.68)
+exp(2.07+1.960*0.68)
+
+
+
+# Question #16
+
+# A study was conducted to determine key predictors of chromosomal fetal abnormalities. 
+# Using the multiple logistic regression model and a cut off selected by the investigator, 
+# 78 fetuses were predicted by the model of having an abnormality. 
+# However, only 14 of the 78 that were predicted to have the abnormality actually did. 
+# Of the 122 fetuses that the model predicted did not have the abnormality, 6 of them actually did. 
+# Construct a 2 by 2 table of these results to help you calculate 
+# the sensitivity of the model using this cutoff.
+
+abnormality_1 <- 14
+abnormality_2 <- 6
+
+total <- abnormality_1 + abnormality_2
+
+
+(abnormality_1 / total)*100
+
+
+
+
+
+
